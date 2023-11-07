72be37063ca02be4568aa4653e8ca9fca59129f4
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-11-07 15:38:43 +0400

    all food data added

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const foodCollection = client.db('restaurentManager').collection('allFoods');
+
+    app.get('/allFoods', async(req, res)=>{
+        const cursor = foodCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
